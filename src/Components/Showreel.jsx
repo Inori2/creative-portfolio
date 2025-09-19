8a8225e8f668aaec327d6d1a992265fb0735c3d9
Inori2 <trannhatsang2000@gmail.com>
2025-09-19 23:30:39 +0700

    Adjust video wrapper translation for mobile view

--- src/Components/Showreel.jsx
+++ src/Components/Showreel.jsx
@@ -35,7 +35,7 @@ export default function Showreel({ isPreloaderDone }) {
           // Mobile settings - NO SCALE
           gsap.set(videoWrapperRef.current, {
             transformOrigin: "top right",
-            translateY: "-80svh",
+            translateY: "-100svh",
           });
         } else if (tablet) {
           // Tablet settings - NO SCALE
