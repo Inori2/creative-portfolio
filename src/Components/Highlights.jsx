708ed9053d82f2aaab635972cef3ae95c87568ca
Inori2 <trannhatsang2000@gmail.com>
2025-10-13 10:22:11 +0700

    adjust navbar postition to sticky

--- src/Components/Highlights.jsx
+++ src/Components/Highlights.jsx
@@ -2,7 +2,9 @@ export default function Highlight() {
   return (
     <>
       <section className="z-10 lg:mt-0">
-        <div className="bg-neutral-950 w-screen h-screen 2xl:mt-20"></div>
+        <div className="bg-neutral-950 w-screen h-screen 2xl:mt-20">
+          <div className="grid grid-cols-2 grid-rows-2 w-full h-full"></div>
+        </div>
       </section>
     </>
   );
